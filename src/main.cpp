fdd02ddf2c82b765a3b52f7ce867f70dafa88e82
x-ion <xf757@nyu.edu>
2022-12-07 23:00:35 -0500

    Add analog inputs

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,37 @@
-/* mbed Microcontroller Library
- * Copyright (c) 2019 ARM Limited
- * SPDX-License-Identifier: Apache-2.0
- */
+// Blink LED
 
+// #include "mbed.h"
+// // Blinking rate in milliseconds
+// #define BLINKING_RATE     200ms
+
+// int main()
+// {
+//     // Initialise the digital pin LED1 as an output
+//     DigitalOut led1(LED1);
+//     DigitalOut led2(LED2);
+//     led2 = !led2;
+//     ThisThread::sleep_for(BLINKING_RATE);
+//     while (true) {
+//         led2 = !led2;
+//         led1 = !led1;
+//         ThisThread::sleep_for(BLINKING_RATE);
+//     }
+// }
+
+
+// Read Analog Inputs
 #include "mbed.h"
 
+// Initialize a pins to perform analog input and digital output functions
+AnalogIn   ain(PA_0);
+DigitalOut dout(LED1);
 
-// Blinking rate in milliseconds
-#define BLINKING_RATE     200ms
-
-
-int main()
+int main(void)
 {
-    // Initialise the digital pin LED1 as an output
-    DigitalOut led1(LED1);
-    DigitalOut led2(LED2);
-
-    led2 = !led2;
-    ThisThread::sleep_for(BLINKING_RATE);
-    while (true) {
-        led2 = !led2;
-        led1 = !led1;
-        ThisThread::sleep_for(BLINKING_RATE);
+    while (1) {
+        ain.set_reference_voltage(1.0);
+        printf("percentage: %3.5f%%\n", ain.read() * 1000000.0f);
+        thread_sleep_for(25);
     }
 }
 
